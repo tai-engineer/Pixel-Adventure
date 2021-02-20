d5a495f28aa3193f67457482820c3fa8dd84bc7a
TaiVan <vcmtspkt@gmail.com>
2021-02-20 21:50:38 +0700

    Update Level 1
    - Add moving background
    - Update Level 1
    - Update PPU of sprites

---
+++ Assets/Scripts/Other/MovingBackground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelAdventure
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class MovingBackground : MonoBehaviour
+    {
+        SpriteRenderer _render;
+        float _distance = 0f;
+
+        [SerializeField] float _speed = 0.5f;
+        Vector2 _origin;
+        void Awake()
+        {
+            _render = GetComponent<SpriteRenderer>();
+
+            _distance = _render.size.y * 0.5f;
+            _origin = transform.position;
+        }
+
+        void Update()
+        {
+            if(Mathf.Abs(transform.position.y - _origin.y) > _distance)
+            {
+                transform.position = _origin;
+            }
+            else
+            {
+                transform.position = new Vector2(_origin.x, transform.position.y - _speed * Time.deltaTime);
+            }
+        }
+    }
+}
