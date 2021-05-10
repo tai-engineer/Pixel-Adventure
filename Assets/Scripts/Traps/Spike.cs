ca86bb6419b2295a0dcfea53f4e7f2bd5e53e3c2
TaiVan <vcmtspkt@gmail.com>
2021-05-10 13:33:37 +0700

    Added Spikde trap prefabs and interfaces
    - interfaces: IDamageable, IDamager

---
+++ Assets/Scripts/Traps/Spike.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour, IDamager
+{
+    [SerializeField] float _damage = default;
+    public void Damage(GameObject obj, float damage)
+    {
+        IDamageable damageable = obj.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            damageable.TakeDamage(damage);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Damage(collision.gameObject, _damage);
+    }
+}
