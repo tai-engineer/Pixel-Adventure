e3e74684951bd5d1bcc9b5e4d9c72bdc9ef529bd
TaiVan <vcmtspkt@gmail.com>
2021-05-15 13:55:51 +0700

    Added enemy Bee prefab
    - Update EnemyController to shoot visible target

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -6,6 +6,7 @@ using UnityEditor;
 #endif
 public class EnemyController : MonoBehaviour
 {
+    [Header("Scanning Settings")]
     [SerializeField] LayerMask _characterLayer = default;
     [SerializeField] float _viewRadius = default;
     [Range(0f, 360f)]
@@ -13,14 +14,32 @@ public class EnemyController : MonoBehaviour
     [Range(0, 360)]
     [SerializeField] float _viewAngle = default;
 
-    List<Transform> _visibleTargets = new List<Transform>();
+    Transform _visibleTarget;
+
+    [Space]
+    [Header("Range Attack")]
+    [SerializeField] Transform _projectilePrefab = default;
+    [SerializeField] float _projectileSpeed = default;
+    [SerializeField] Transform _shootingOrigin = default;
+    [SerializeField] BulletPool _bulletPool = default;
+    [SerializeField] float _rangeDamage = default;
+    [SerializeField] float _timeBetweenShots = default;
+    float _timer;
+    bool _firstShot;
 
     void Update()
     {
         ScanForPlayer();
+        ShootTarget();
     }
+
+    /// <summary>
+    /// Shoot first target in range
+    /// </summary>
     void ScanForPlayer()
     {
+        _visibleTarget = null;
+
         Vector2 forward = GetComponent<SpriteRenderer>().flipX ? Vector2.left : Vector2.right;
         forward = Quaternion.Euler(0f, 0f, _viewAngle) * forward;
 
@@ -34,12 +53,34 @@ public class EnemyController : MonoBehaviour
 
             if(Vector2.Angle(forward, dirToTarget) < _viewPOV /2)
             {
-                _visibleTargets.Add(target);
                 Debug.DrawLine(transform.position, target.position);
+                _visibleTarget = target;
             }
         }
     }
+    void ShootTarget()
+    {
+        if (_visibleTarget == null)
+            return;
 
+        if (_timer > _timeBetweenShots || !_firstShot)
+        {
+            SpawnProjectiles(_shootingOrigin.position, _visibleTarget.position);
+            _firstShot = true;
+            _timer = 0f;
+        }
+        else
+        {
+            _timer += Time.deltaTime;
+        }
+    }
+
+    void SpawnProjectiles(Vector2 origin, Vector2 target)
+    {
+        BulletObject bulletObj = _bulletPool.Pop(_shootingOrigin.position);
+
+        bulletObj.rb2D.velocity = (target - origin).normalized * _projectileSpeed;
+    }
 #if UNITY_EDITOR
     void OnDrawGizmosSelected()
     {
