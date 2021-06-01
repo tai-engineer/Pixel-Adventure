bd2d50bd33678978deb49d26f92a7abc3a085a59
TaiVan <vcmtspkt@gmail.com>
2021-06-01 13:02:57 +0700

    Updated Brick script and added brick editor

--- Assets/Scripts/Environment/Brick.cs
+++ Assets/Scripts/Environment/Brick.cs
@@ -1,22 +1,32 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Brick : MonoBehaviour
 {
     const float BOUNCE_DISTANCE = 1f;
-    [SerializeField] float _bounceSpeed = 7f;
-    [SerializeField] CoinPool _pool = default;
     [SerializeField] bool _canSpawn = false;
+    [SerializeField] float _bounceSpeed = 7f;
+    [SerializeField] CoinPool _coinPool = default;
+    [SerializeField] int _noOfCoin = default;
+    [SerializeField] Sprite _disabledSprite = default;
 
     bool _canBounce = false;
+    bool _disable = false;
     int _direction = 1; // 1: upward, -1: downward
     float _interpolation;
 
     Vector2 _origin;
     float _targetPositionY;
+    int _coinRemain;
+
+    SpriteRenderer _renderer;
     void Awake()
     {
+        _renderer = GetComponent<SpriteRenderer>();
+
         _origin = transform.position;
         _targetPositionY = _origin.y + BOUNCE_DISTANCE;
+        _coinRemain = _noOfCoin;
     }
     void Update()
     {
@@ -25,6 +35,9 @@ public class Brick : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (_disable)
+            return;
+
         ContactPoint2D[] contacts = new ContactPoint2D[1];
         collision.GetContacts(contacts);
 
@@ -32,16 +45,15 @@ public class Brick : MonoBehaviour
         if (contacts[0].point.y < transform.position.y)
         {
             _canBounce = true;
-
-            if (_canSpawn)
-            {
-                _pool.Pop(transform.position);
-            }
+            SpawnPrefab();
         }
     }
     void Bounce()
     {
-        if(_canBounce)
+        if (_disable)
+            return;
+
+        if (_canBounce)
         {
             _interpolation += _direction * _bounceSpeed * Time.deltaTime;
             _interpolation = Mathf.Clamp01(_interpolation);
@@ -59,7 +71,27 @@ public class Brick : MonoBehaviour
             {
                 _direction = 1;
                 _canBounce = false;
+
+                if (_coinRemain <= 0)
+                {
+                    Disable();
+                }
             }
         }
+    }
+    void SpawnPrefab()
+    {
+        if (_canSpawn && _coinRemain > 0)
+        {
+            _coinPool.Pop(transform.position);
+            _coinRemain--;
+        }
+    }
+    void Disable()
+    {
+        if (!_canSpawn)
+            return;
+        _renderer.sprite = _disabledSprite;
+        _disable = true;
     }
 }
---
+++ Assets/Scripts/Environment/Editor/BrickEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Brick))]
+public class BrickEditor : Editor
+{
+    SerializedProperty bounceSpeed;
+    SerializedProperty canSpawn, coinPool, noOfCoin, disabledSprite;
+
+    void OnEnable()
+    {
+        bounceSpeed = serializedObject.FindProperty("_bounceSpeed");
+        canSpawn = serializedObject.FindProperty("_canSpawn");
+        coinPool = serializedObject.FindProperty("_coinPool");
+        noOfCoin = serializedObject.FindProperty("_noOfCoin");
+        disabledSprite = serializedObject.FindProperty("_disabledSprite");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(disabledSprite);
+        EditorGUILayout.PropertyField(bounceSpeed);
+
+        EditorGUILayout.BeginHorizontal();
+        canSpawn.isExpanded = EditorGUILayout.Foldout(canSpawn.isExpanded, new GUIContent("Has prefab to spawn"));
+        EditorGUILayout.EndHorizontal();
+
+        if(canSpawn.isExpanded)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(coinPool);
+            EditorGUILayout.PropertyField(noOfCoin);
+            EditorGUI.indentLevel--;
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
